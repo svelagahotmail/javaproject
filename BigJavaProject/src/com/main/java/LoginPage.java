1639c9c90dee3deb1e1eb2632c4f302c2b2c0edb
svelaga <svelaga@hotmail.com>
2022-02-03 20:56:47 -0600

    Pushing Thursday evening changes

--- BigJavaProject/src/com/main/java/LoginPage.java
+++ BigJavaProject/src/com/main/java/LoginPage.java
@@ -5,7 +5,7 @@ public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to login page");
 		
-		System.out.println("good bye All");
+		System.out.println("bye all -- thursday eveningfdasfdasf");
 
 	}
 
